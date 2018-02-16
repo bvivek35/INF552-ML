1ad64ed6f4754a3076abef59818c9e8aa0b4b4cd
Vivek Bharadwaj <bvivek35@gmail.com>
2018-02-16 09:01:34 -0800

    Starting Assignment-2

---
+++ HW2/Readme.md
@@ -0,0 +1,6 @@
+#### Clustering
+Implementation of Clustering using KMeans(hard membership) and Gaussian Mixture Models(soft membership). Uses NumPy array for modeling data points in ND space.
+1. question/ - contains the zip file and the pdf containing the exercise
+2. code/ - contains the source code for implementation
+3. data/ - contains the dataset for the exercise
+4. report/ - contains the report for this assignment
---
+++ HW2/code/KMeansClustering.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+'''
+    Class: INF552 at USC
+    Submission Member: Vivek Bharadwaj <vivekb> <vivekb@usc.edu>
+    This is a python implementation of Clustering using K-Means Algorithm.
+    The Algorithm will accept numpy array of array: shape -> (XXXX, 2)
+'''
+
+__author__ = 'Vivek Bharadwaj'
+__email__ = 'vivekb@usc.edu'
+__version__ = '1.0'
+
+# Imports
+import math
+from collections import namedtuple
+import os # For checking PY_USER_LOG environ var for logging
+import logging
+import pprint
+# End imports
+
+# Setup logging
+LOG_LEVEL = logging.getLevelName(os.environ.get('PY_USER_LOG', 'CRITICAL'))
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
+logger = logging.getLogger(__name__)
+
+# Declare decorators
+def logArgs(logger=None):
+    import pprint
+    def logArgsWrapped(fn):
+        def loggedFn(*args, **kwargs):
+            logMsg = 'Calling {0}({1}, {2})'.format(fn.__name__, pprint.pformat(args), pprint.pformat(kwargs))
+            if logger:
+                logger.debug(logMsg)
+            elif print:
+                print(logMsg)
+            
+            return fn(*args, **kwargs)
+        
+        return loggedFn
+    return logArgsWrapped
+
+# Declare types
+Cluster = namedtuple('Cluster', ['clusterNumber', 'centroid', 'pts'])
+ClusterResult = namedtuple('ClusterResult', ['clusters', 'clusterIndex'])
+
+@logArgs(logger)
+def KMeansCluster(pts, K):
+    '''
+    Args:
+        pts: data points to cluster
+        K: number of clusters to form
+    Returns:
+        ClusterResult
+    '''
+    return ClusterResult([Cluster(0, pts[0], pts)], lambda x: 0)
+
+if __name__ == '__main__':
+    # Imports
+    import sys
+    from numpy import genfromtxt
+    # End imports
+
+    HELP_TEXT = 'USAGE: {0} <ClustersDataCSVFile> <NumberOfClusters>'
+    if len(sys.argv) != 3:
+        print(HELP_TEXT.format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        inFile = sys.argv[1]
+        NClusters = int(sys.argv[2])
+    
+    pts = genfromtxt(inFile, delimiter = ',')
+
+    clusterResult = KMeansCluster(pts, K = NClusters)
+    for cluster in clusterResult.clusters:
+        print('Cluster {0} - centroid {1}'.format(cluster.clusterNumber, cluster.centroid))
