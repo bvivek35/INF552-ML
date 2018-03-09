62d95437ae765f48786fd1aafc3e90c17f7fd2ff
Vivek Bharadwaj <bvivek35@gmail.com>
2018-03-08 23:39:11 -0800

    Added moudule level docstring for LinearRegression

--- HW4/code/LinearRegression.py
+++ HW4/code/LinearRegression.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+
+'''
+    Class: INF552 at USC
+    Submission Member:
+                        Vivek Bharadwaj <vivekb> <vivekb@usc.edu>
+                        Shushyam Malige Sharanappa <maligesh> <maligesh@usc.edu>
+                        Raveena Mathur <raveenam> <raveenam@usc.edu>
+    This is a python implementation of Linear Regression.
+'''
+
 import pandas as pd
 import os
 import numpy as np
